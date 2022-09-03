b9ff52e4b183dd2114fe40324924710f13cd9ba4
Phoom Punpeng <phoomp14@gmail.com>
2022-09-03 21:27:00 +0700

    Added number typechecking

--- graphExecution/typeCheck.c
+++ graphExecution/typeCheck.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "graph.h"
+#include "errors.h"
+
+// Return true if the number, passed as char*, has "-" at index 0
+bool isNegative(char* number) {
+    int size = sizeof(number);
+
+    if (size < sizeof(char)) {
+        raiseCriticalErrorAndExit(-1, "Invalid pointer to number string");
+    }
+
+    if (number[0] == '-') {
+        if (strnlen(number, 10)) {
+            raiseCriticalErrorAndExit(-1, "Invalid number format");
+        }
+        printDebug("Negative number");
+        return true;
+    }
+    else {
+        printDebug("Positive number");
+        return false;
+    }
+}
+
 
 // Returns the index of the decimal place if the string is a valid number
 // Else, returns -1
@@ -15,13 +39,14 @@ int typeCheckStringToNum(char* number, int numDigits) {
         else if (i == 0 && num == 45) continue;
         else if (num == 46 && !decimalFound) {
             if (i == 0) {
-                printf("Shorthand notations are not allowed.");
-                return -1;
+                raiseCriticalErrorAndExit(-1, "Shorthand notations are not allowed.");
             }
             decimalFound = true;
             decimal = i;
         }
-        else return -1;
+        else {
+            raiseCriticalErrorAndExit(-1, "Invalid number format");
+        }
     }
 
     return decimal;
