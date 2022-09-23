8f736b52c21aa553c96692bbf989ec2797b791c2
Phoom Punpeng <phoomp14@gmail.com>
2022-09-23 14:38:22 +0700

    Rewrote some code in C++

---
+++ casio/bin/cpp/neg.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+pair<double, double> coefficient(pair<double, double> numerator, pair<double, double> denominator) {
+    return pair<double, double>();
+}
+
+int main() { 
+    vector<int> a = f();
+    return 0;
+}
